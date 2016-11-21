1691c9615d6ef0509c2d7bc0e3d7c3310d32a5ca
Pedro Vitorino <pedrovitorino.s.p@gmail.com>
2016-11-21 01:38:07 -0200

    fixed issues

--- RqstHandler.py
+++ RqstHandler.py
@@ -38,15 +38,18 @@ class RqstHandler:
         if(nodes == []):
             if(current_node.data == None and data != ''):
                 current_node.data = data
-                return "Success 200"
+                return self.set_return_message(200, "ADD", current_node)
             else:
-                return "Error 404"
+                return self.set_return_message(400, "ADD", current_node)
 
         if(self.search_name(current_node, nodes[0]) != None):
             aux = nodes[0]
             del(nodes[0])
             n_path = path.split("/", 2)
-            n_path = n_path[1]
+            if(len(n_path) > 2):
+                n_path = "/" + n_path[2]
+            else:
+                n_path = ""
             return self.do_add(data, n_path, self.search_name(current_node, aux))
         else:
             self.incrementCounter()
@@ -55,7 +58,10 @@ class RqstHandler:
             aux = nodes[0]
             del(nodes[0])
             n_path = path.split("/", 2)
-            n_path = n_path[1]
+            if(len(n_path) > 2):
+                n_path = "/" + n_path[2]
+            else:
+                n_path = ""
             return self.do_add(data, n_path, self.search_name(current_node, aux))
 
     def do_get(self, path):
@@ -69,12 +75,12 @@ class RqstHandler:
                 current_node = self.search_name(current_node, nodes[counter])
                 counter += 1
             else:
-                return "Error 404"
+                return self.set_return_message(404, "GET", current_node)
 
         if(current_node.data != None):
-            return "Success 200"
+            return self.set_return_message(200, "GET", current_node)
         else:
-            return "Error 404"
+            return self.set_return_message(404, "GET", current_node)
 
     def do_update(self, data, path, current_node):
         nodes = self.clean_resource(path)
@@ -85,20 +91,23 @@ class RqstHandler:
         if(nodes == []):
             if(current_node.data != None and data != ''):
                 self.alter_data(current_node, data)
-                return "Success 200"
+                return self.set_return_message(200, "UPDATE", current_node)
             else:
-                return "Error 400"
+                return self.set_return_message(400, "UPDATE", current_node)
 
         if(self.search_name(current_node, nodes[0]) != None):
             aux = nodes[0]
             del(nodes[0])
             n_path = path.split("/", 2)
-            n_path =  "/" + n_path[1]
+            if(len(n_path) > 2):
+                n_path = "/" + n_path[2]
+            else:
+                n_path = ""
             return self.do_update(data, n_path, self.search_name(current_node, aux))
         else:
-            return "Error 404"
+            self.set_return_message(404, "UPDATE", current_node)
 
-    def do_update_version(self, data, path, version, current_node):
+    def do_update_version(self, data, version, path, current_node):
         nodes = self.clean_resource(path)
 
         if(current_node == None):
@@ -107,18 +116,21 @@ class RqstHandler:
         if(nodes == []):
             if(current_node.data != None and data != '' and current_node.version == version):
                 self.alter_data(current_node, data)
-                return "Success 200"
+                return self.set_return_message(200, "UPDATE+VERSION", current_node)
             else:
-                return "Error 400"
+                return self.set_return_message(400, "UPDATE+VERSION", current_node)
 
         if(self.search_name(current_node, nodes[0]) != None):
             aux = nodes[0]
             del(nodes[0])
             n_path = path.split("/", 2)
-            n_path =  "/" + n_path[2]
-            return self.do_update_version(data, n_path, self.search_name(current_node, aux))
+            if(len(n_path) > 2):
+                n_path = "/" + n_path[2]
+            else:
+                n_path = ""
+            return self.do_update_version(data, version, n_path, self.search_name(current_node, aux))
         else:
-            return "Error 404"
+            return self.set_return_message(404, "UPDATE+VERSION", current_node)
 
     def do_delete(self, path):
         nodes = self.clean_resource(path)
@@ -132,10 +144,10 @@ class RqstHandler:
                 current_node = self.search_name(current_node, nodes[counter])
                 counter += 1
             else:
-                return "Error 404"
+                return self.set_return_message(404, "DELETE", current_node)
 
         self.delete_child(current_node, self.search_name(current_node, nodes[counter]))
-        return "Sucess 200"
+        return self.set_return_message(200, "DELETE", current_node)
 
     def do_delete_version(self, path, version):
         nodes = self.clean_resource(path)
@@ -149,13 +161,13 @@ class RqstHandler:
                 current_node = self.search_name(current_node, nodes[counter])
                 counter += 1
             else:
-                return "Error 404"
+                return self.set_return_message(404, "DELETE+VERSION", current_node)
 
-        if(search_name(current_node, nodes[counter]).version != int(version)):
-            return "Error 403"
+        if(self.search_name(current_node, nodes[counter]).version != int(version)):
+            return self.set_return_message(400, "DELETE+VERSION", current_node)
 
         self.delete_child(current_node, self.search_name(current_node, nodes[counter]))
-        return "Sucess 200"
+        return self.set_return_message(200, "VERSION", current_node)
 
     def do_list(self, path):
         nodes = self.clean_resource(path)
@@ -167,12 +179,12 @@ class RqstHandler:
                 current_node = self.search_name(current_node, nodes[counter])
                 counter += 1
             else:
-                return "Error 404"
+                return self.set_return_message(404, "LIST", current_node)
 
         if(current_node.children != None):
-            return "Success 200"
+            return self.set_return_message(200, "LIST", current_node)
         else:
-            return "Error 404"
+            return self.set_return_message(404, "LIST", current_node)
 
     def incrementCounter(self):
         self.countId.counter += 1
@@ -217,7 +229,7 @@ class RqstHandler:
     def get_children(self, node, returnList):
         for child in node.children:
             returnList.append(self.getNodeById(child).name)
-            get_children(self.getNodeById(child), returnList)
+            self.get_children(self.getNodeById(child), returnList)
 
         return returnList
 
@@ -225,106 +237,75 @@ class RqstHandler:
         for listN in self.nodeList.lista:
             if(listN.id == nid):
                 return listN
-    #  def set_return_message(self, ret_request, request, node):
-    #     message = ''
-    #     if request == "POST":
-    #         if(ret_request == 200):
-    #             l1 = "POST 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4
-    #         elif(ret_request == 400):
-    #             message = "POST 400 Empty Body or File Already Exists"
-    #         return message
-    #     if request == "ADD":
-    #         if(ret_request == 200):
-    #             l1 = "ADD 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4
-    #         elif(ret_request == 400):
-    #             message = "ADD 400 Empty Body or File Already Exists"
-    #         return message
-    #     elif request == "GET":
-    #         if(ret_request == 200):
-    #             l1 = "GET 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4+node.data
-    #         elif(ret_request == 404):
-    #             message = "GET 404 File not found"
-    #         return message
-    #     elif request == "PUT":
-    #         if(ret_request == 200):
-    #             l1 = "PUT 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4
-    #         elif(ret_request == 400):
-    #             message = "PUT 400 Empty Body or File Doen't Exists"
-    #         elif(ret_request == 404):
-    #             message = "PUT 404 Invalid path"
-    #         return message
-    #     elif request == "UPDATE":
-    #         if(ret_request == 200):
-    #             l1 = "UPDATE 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4
-    #         elif(ret_request == 400):
-    #             message = "UPDATE 400" + " Empty Body or File Doen't Exists"
-    #         elif(ret_request == 404):
-    #             message = "UPDATE 404 Invalid path"
-    #         return message
-    #     elif request == "HEAD":
-    #         if(ret_request == 200):
-    #             l1 = "HEAD 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4
-    #         elif(ret_request == 404):
-    #             message = "HEAD 404 File not found"
-    #         return message
-    #     elif request == "DELETE":
-    #         if(ret_request == 200):
-    #             message = "DELETE 200 OK" + self._CRLF
-    #         elif(ret_request == 404):
-    #             message = "DELETE 404 File not found"
-    #         return message
-    #     elif request == "LIST":
-    #         if(ret_request == 200):
-    #             l1 = "LIST 200 OK" + self._CRLF
-    #             l2 = "Children:  " + str(node.get_children(node, [])) + self._CRLF
-    #             message = l1+l2
-    #         elif(ret_request == 404):
-    #             message = "LIST 404 Node not found"
-    #         return message
-    #     elif re.match("DELETE\+[0-9]+", request):
-    #         if(ret_request == 200):
-    #             message = "DELETE+VERSION 200 OK" + self._CRLF
-    #         elif(ret_request == 404):
-    #             message = "DELETE+VERSION 404 File not found"
-    #         elif(ret_request == 403):
-    #             message = "DELETE+VERSION 403 Invalid version"
-    #         return message
-    #     elif re.match("UPDATE\+[0-9]+", request):
-    #         if(ret_request == 200):
-    #             l1 = "UPDATE+VERSION 200 OK" + self._CRLF
-    #             l2 = "Version:  " + str(node.version) + self._CRLF
-    #             l3 = "Creation:  " + node.creation.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             l4 = "Modification:  " + node.modification.strftime("%Y-%m-%d %H:%M:%S") + self._CRLF
-    #             message = l1+l2+l3+l4
-    #         elif(ret_request == 400):
-    #             message = "PUT 400 Empty Body or File Doen't Exists or Version Invalid"
-    #         elif(ret_request == 404):
-    #             message = "PUT 404 Invalid path"
-    #         return message
-    #     else:
-    #         print("\n Invalid Method\n")
-    #         return
+
+    def set_return_message(self, ret_request, request, node):
+        message = ''
+        if request == "ADD":
+            if(ret_request == 200):
+                l1 = "ADD 200 OK"# + self._CRLF
+                l2 = "Version:  " + str(node.version) #+ self._CRLF
+                l3 = "Creation:  " + node.creation #+ self._CRLF
+                l4 = "Modification:  " + node.modification #+ self._CRLF
+                message = l1+l2+l3+l4
+            elif(ret_request == 400):
+                message = "ADD 400 Empty Body or File Already Exists"
+            return message
+        elif request == "GET":
+            if(ret_request == 200):
+                l1 = "GET 200 OK"# + self._CRLF
+                l2 = "Version:  " + str(node.version)# + self._CRLF
+                l3 = "Creation:  " + node.creation# + self._CRLF
+                l4 = "Modification:  " + node.modification# + self._CRLF
+                message = l1+l2+l3+l4+node.data
+            elif(ret_request == 404):
+                message = "GET 404 File not found"
+            return message
+        elif request == "UPDATE":
+            if(ret_request == 200):
+                l1 = "UPDATE 200 OK"# + self._CRLF
+                l2 = "Version:  " + str(node.version)# + self._CRLF
+                l3 = "Creation:  " + node.creation# + self._CRLF
+                l4 = "Modification:  " + node.modification# + self._CRLF
+                message = l1+l2+l3+l4
+            elif(ret_request == 400):
+                message = "UPDATE 400" + " Empty Body or File Doen't Exists"
+            elif(ret_request == 404):
+                message = "UPDATE 404 Invalid path"
+            return message
+        elif request == "DELETE":
+            if(ret_request == 200):
+                message = "DELETE 200 OK"# + self._CRLF
+            elif(ret_request == 404):
+                message = "DELETE 404 File not found"
+            return message
+        elif request == "LIST":
+            if(ret_request == 200):
+                l1 = "LIST 200 OK"# + self._CRLF
+                l2 = "Children:  " + str(self.get_children(node, []))# + self._CRLF
+                message = l1+l2
+            elif(ret_request == 404):
+                message = "LIST 404 Node not found"
+            return message
+        elif request == "DELETE+VERSION":
+            if(ret_request == 200):
+                message = "DELETE+VERSION 200 OK"# + self._CRLF
+            elif(ret_request == 404):
+                message = "DELETE+VERSION 404 File not found"
+            elif(ret_request == 403):
+                message = "DELETE+VERSION 403 Invalid version"
+            return message
+        elif request == "UPDATE+VERSION":
+            if(ret_request == 200):
+                l1 = "UPDATE+VERSION 200 OK"# + self._CRLF
+                l2 = "Version:  " + str(node.version)# + self._CRLF
+                l3 = "Creation:  " + node.creation# + self._CRLF
+                l4 = "Modification:  " + node.modification# + self._CRLF
+                message = l1+l2+l3+l4
+            elif(ret_request == 400):
+                message = "PUT 400 Empty Body or File Doen't Exists or Version Invalid"
+            elif(ret_request == 404):
+                message = "PUT 404 Invalid path"
+            return message
+        else:
+            print("\n Invalid Method\n")
+            return
